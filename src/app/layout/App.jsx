3281145fd8b76324cafcc7e9e42f2cb2d81352c1
Anthony Walker <anthonyaustinwalker@gmail.com>
2018-09-13 22:21:30 -0700

    adding logo, EventDashboard component and Navbar component

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
+
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import Navbar from '../../features/nav/Navbar/Navbar';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Rendezvous</h1>
-      </div>
-    );
+    return [
+      <Navbar />,
+      <Container className="main">
+        <EventDashboard />
+      </Container>
+    ];
   }
 }
 
